3cbbb193ba42e59f378ee1f80ce7a721bd842dfe
goddemi <goddemi0124@gmail.com>
2022-12-07 23:27:49 +0900

    Add : components total test

--- youtube-tdd-components-all/src/components/tests/RelatedVideos.test.js
+++ youtube-tdd-components-all/src/components/tests/RelatedVideos.test.js
@@ -1,61 +1,59 @@
-import { Route } from 'react-router-dom';
-import { withAllContexts, withRouter } from '../../tests/utils';
+import { Route } from "react-router-dom";
+import { withAllContexts, withRouter } from "../../tests/utils";
 import {
   screen,
   render,
   waitFor,
   waitForElementToBeRemoved,
-} from '@testing-library/react';
-import { fakeVideos } from '../../tests/videos';
-import RelatedVideos from '../RelatedVideos';
+} from "@testing-library/react";
+import { fakeVideos } from "../../tests/videos";
+import RelatedVideos from "../RelatedVideos";
 
-describe('RelatedVideos', () => {
+describe("RelatedVideos", () => {
   const fakeYoutube = {
     relatedVideos: jest.fn(),
   };
 
-  afterEach(() => fakeYoutube.relatedVideos.mockReset());
+  afterEach(() => fakeYoutube.relatedVideos.mockReset()); //하나만 잇으면 안해도 되는데 여러개 있으면 테스트끼리 영향을 줄 수 있으니 해줘야 한다.
 
-  it('renders correctly', async () => {
+  it("renders correctly", async () => {
     fakeYoutube.relatedVideos.mockImplementation(() => fakeVideos);
     const { asFragment } = renderRelatedVideos();
 
-    await waitForElementToBeRemoved(screen.queryByText('Loading...'));
+    await waitForElementToBeRemoved(screen.queryByText("Loading..."));
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders related videos correctly', async () => {
+  it("renders related videos correctly", async () => {
     fakeYoutube.relatedVideos.mockImplementation(() => fakeVideos);
     renderRelatedVideos();
 
-    expect(fakeYoutube.relatedVideos).toHaveBeenCalledWith('id');
-    await waitFor(() =>
-      expect(screen.getAllByRole('listitem')).toHaveLength(fakeVideos.length)
-    );
+    expect(fakeYoutube.relatedVideos).toHaveBeenCalledWith("id");
+    expect(screen.findAllByRole("listitem"));
   });
 
-  it('renders loading', () => {
+  it("renders loading", () => {
     fakeYoutube.relatedVideos.mockImplementation(() => fakeVideos);
     renderRelatedVideos();
 
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
 
-  it('renders error', async () => {
+  it("renders error", async () => {
     fakeYoutube.relatedVideos.mockImplementation(() => {
-      throw new Error('error');
+      throw new Error("error");
     });
 
     renderRelatedVideos();
     await waitFor(() => {
-      expect(screen.getByText('Something is wrong 😖')).toBeInTheDocument();
+      expect(screen.getByText("Something is wrong 😖")).toBeInTheDocument();
     });
   });
 
   function renderRelatedVideos() {
     return render(
       withAllContexts(
-        withRouter(<Route path='/' element={<RelatedVideos id='id' />} />),
+        withRouter(<Route path="/" element={<RelatedVideos id="id" />} />),
         fakeYoutube
       )
     );
